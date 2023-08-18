ba5a52aa08cf766891240b311c4f1726bd98dcca
jayakarthik-jk <karthick72002@gmail.com>
2023-08-18 22:23:53 +0530

    added layout

--- _eslintrc.cjs
+++ _eslintrc.cjs
@@ -13,17 +13,6 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
 
